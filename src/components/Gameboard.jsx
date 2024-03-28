ff65d1d2e2c0ed540515015a6c84b208816255f6
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-03-28 18:51:14 -0400

    Add Gameboard, Card components

---
+++ src/components/Gameboard.jsx
@@ -0,0 +1,20 @@
+import Card from "./Card"
+
+export default function Gameboard( { setScore, setTopScore } ) {
+    return (
+        <>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </>
+    )
+}
