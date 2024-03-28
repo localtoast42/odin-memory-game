d9e83a13265f8ea9766448c4a46ff2bd8b3b163b
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-03-28 18:46:36 -0400

    Add Scoreboard component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Scoreboard from './components/Scoreboard';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
 
   return (
     <>
+      <div>
+        <Scoreboard 
+          score={score}
+          topScore={topScore}
+        />
+        <div>
+          <h1>Memory Game</h1>
+          <h2>Test your memory.</h2>
+        </div>
+      </div>
     </>
   )
 }
