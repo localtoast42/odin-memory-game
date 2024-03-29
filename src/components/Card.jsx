1b3c3e5c886d6b7961cf8ab16b7fa84b4426dda7
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-03-29 11:45:51 -0400

    Add basic styling to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,12 @@
+import '../styles/Card.css'
+
 export default function Card() {
     return (
         <>
+            <div className="card">
+                <img src="" alt="" />
+                <h3>Card</h3>
+            </div>
         </>
     )
 }
