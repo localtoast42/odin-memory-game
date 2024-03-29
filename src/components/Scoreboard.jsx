3576e6ceab2566dd21a93c6b15ecc4f9f6d09398
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-03-29 14:35:25 -0400

    Add styling for scoreboard elements

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,9 +1,13 @@
+import '../styles/Scoreboard.css'
+
 export default function Scoreboard( { score, topScore} ) {
     return (
         <>
             <div className="scoreboard">
-                <span>Current score: {score}</span>
-                <span>Top score: {topScore}</span>
+                <h3>Top score:</h3>
+                <h3 className='score'>{topScore}</h3>
+                <h3>Current score:</h3>
+                <h3 className='score'>{score}</h3>
             </div>
         </>
     )
