a0580fbc2a91dee69df011bca5a5e0da010471aa
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-03-29 15:50:48 -0400

    Add function to fetch PokeAPI info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,25 @@
 import '../styles/Card.css'
+import { useState, useEffect } from 'react'
 
 export default function Card({ cardId, onClick }) {
+
+    const pokemonName = 'pikachu';
+    const [photoURL, setPhotoURL] = useState('');
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
+            .then((result) => {
+                return result.json();
+            })
+            .then((data) => {
+                setPhotoURL(data['sprites']['other']['official-artwork']['front_default']);
+            });
+    }, []);
+
     return (
         <button className="card" onClick={onClick} value={cardId}>
-            Card
+            <img src={photoURL} alt='' height='150px'/>
+            {pokemonName.toUpperCase()}
         </button>
     )
 }
