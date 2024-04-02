f0a3426a98438fdefd0bc995ea06095fd1196aa0
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-04-02 12:24:32 -0400

    Add shuffleCards function

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -21,6 +21,13 @@ export default function Gameboard( { score, setScore, topScore, setTopScore } ) 
         }
     }
 
+    function shuffleCards(cardList) {
+        for (let i = cardList.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [cardList[i], cardList[j]] = [cardList[j], cardList[i]];
+        }
+    }
+
     return (
         <>
             <div className='gameboard'>
